9122cd35e46b92d297120d92b6deab742ec1a938
leonhard <leonhard@mpdl.mpg.de>
2020-03-22 16:37:57 +0100

    feat FDMR-19:  Improved scripts & css load management

--- dist-start-setup/start.js
+++ dist-start-setup/start.js
@@ -1,34 +1,42 @@
 const queue = [
-    './css/app.css.preload',
-    './app.js.preload',
+    './config/dev.js',
+    // './css/app.css.preload',
+    // './app.js.preload',
     './lib/lodash/lodash.min.js',
     './lib/jquery/jquery.3.4.1.min.js',
     './lib/jquery/jquery-ui.min.js',
     './css/app.css',
-    './config/dev.js',
     './app.js'
 ].reverse()
+
 const assign = {
     js: ['script', 'src'],
     css: ['link', 'href', ['rel', 'stylesheet']],
     js_pre: ['script', 'src', ['rel', 'preload'], ['as', 'script']],
     css_pre: ['link', 'href', ['rel', 'preload'], ['as', 'style']]
 }
-while (queue.length) {
-    let target = queue.pop().split('.')
-    let ending = target.pop()
-    let attrs = assign[ending]
-    if (ending === 'preload') {
-        ending = target.pop()
-        attrs = assign[`${ending}_pre`]
+
+const loadNext = () => {
+    if (queue.length) {
+        let target = queue.pop().split('.')
+        let ending = target.pop()
+        let attrs = assign[ending]
+        if (ending === 'preload') {
+            ending = target.pop()
+            attrs = assign[`${ending}_pre`]
+        }
+        attrs = [...attrs].reverse()
+        target = `${target.join('.')}.${ending}`
+        let elm = document.createElement(attrs.pop())
+        elm.setAttribute(attrs.pop(), target)
+        while (attrs.length) {
+            const attr = attrs.pop()
+            elm.setAttribute(attr[0], attr[1])
+        }
+        document.head.appendChild(elm)
+        console.log('INDEX: elm load = ', elm)
+        elm.onload = () => loadNext()
     }
-    attrs = [...attrs].reverse()
-    target = `${target.join('.')}.${ending}`
-    let elm = document.createElement(attrs.pop())
-    elm.setAttribute(attrs.pop(), target)
-    while (attrs.length) {
-        const attr = attrs.pop()
-        elm.setAttribute(attr[0], attr[1])
-    }
-    document.head.appendChild(elm)
 }
+
+loadNext()
